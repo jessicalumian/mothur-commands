568c00b31db9afe2b8554ccff9c277416fff823f
jessicamizzi <jessica.mizzi@gmail.com>
2016-09-27 20:39:26 -0700

    fixed syntax

--- de-interleavereads.sh
+++ de-interleavereads.sh
@@ -2,5 +2,8 @@
 
 for i in *.fastq.gz
 do
-    split-paired-reads.py $i
+    ref_name="$(basename $i .fastq.gz)"
+    echo "reference name is $ref_name"
+    split-paired-reads.py $i -1 ${ref_name}_R1_.fastq.gz -2 ${ref_name}_R2_.fastq.gz --gzip
+    rm $i
 done
